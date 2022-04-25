eedac7a7087c0f3b147f6312ec390bf6630c64a7
GiuliannT <giuliconti1@gmail.com>
2022-04-24 21:27:11 -0300

    changes LoginForm

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
-import showPassword from './svg/showPassword.svg';
-import hidePassword from './svg/hidePassword.svg';
-import errorPassword from './svg/errorPassword.svg';
-import clearText from './svg/clearText.svg';
 import './LoginForm.css';
 
 export const LoginForm = ({ logged, showFormSignUp, studentOrTeacher, setStudentOrTeacher }) => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const [isVisiblePassword, setIsVisiblePassword] = useState(false);
-    const [typePassword, setTypePassword] = useState('password');
     const [email, setEmail] = useState('');
 
     const handleSubmit = e => {
@@ -52,15 +46,8 @@ export const LoginForm = ({ logged, showFormSignUp, studentOrTeacher, setStudent
     const handleEmailChange = e => {
         setEmail(e.target.value);
     }
-    const clearEmail = () => {
-        setEmail('');
-    }
     const handlePasswordChange = e => {
         setPassword(e.target.value);
-    }
-    const handleVisiblePassword = () => {
-        setIsVisiblePassword(!isVisiblePassword);
-        typePassword === 'password' ? setTypePassword('text') : setTypePassword('password');
     }
     const handleTypeChange = e => {
         const value = e.target.value
@@ -70,26 +57,16 @@ export const LoginForm = ({ logged, showFormSignUp, studentOrTeacher, setStudent
     return (
         <form className='login-form-container' onSubmit={handleSubmit}>
             {
-                (error.length > 0) && (<div className="login-form-error"><img src={errorPassword} alt='error password'></img>{error}</div>)
+                (error.length > 0) && (<div className="login-form-error">{error}</div>)
             }
             <div className='login-form-title'>Please fill in your unique admin login details below</div>
             <label htmlFor='email' className='login-form-input-label'>Email address</label>
             <div>
                 <input name='email' autoComplete='off' id='email' type='email' className='login-form-input' value={email} onChange={handleEmailChange} />
-                {
-                    email
-                        ? <i className='eye-password'><img src={clearText} alt='show password' onClick={clearEmail} /></i>
-                        : <i className='eye-password'></i>
-                }
             </div>
             <label htmlFor='password' className='login-form-input-label'>Password</label>
             <div>
-                <input name='password' autoComplete='off' id='password' value={password} type={typePassword} className='login-form-input' onChange={handlePasswordChange} />
-                {
-                    isVisiblePassword
-                        ? <i className='eye-password'><img src={showPassword} alt='show password' onClick={handleVisiblePassword} /></i>
-                        : <i className='eye-password'><img src={hidePassword} alt='show password' onClick={handleVisiblePassword} /></i>
-                }
+                <input name='password' autoComplete='off' id='password' value={password} className='login-form-input' onChange={handlePasswordChange} />
             </div>
             <div className='type'>
                 <div className='log-in-link' onClick={showFormSignUp}>Sign Up</div>
